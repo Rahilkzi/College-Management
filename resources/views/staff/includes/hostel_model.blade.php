bed5980ef5c74985c4ed5c6473e0bf7a5be36279
Rahilkazi <rahilkazi66@gmail.com>
2025-02-03 23:26:56 +0530

    push

---
+++ resources/views/staff/includes/hostel_model.blade.php
@@ -0,0 +1,21 @@
+<!-- Modal -->
+<div class="modal fade" id="activeAgain" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+
+        <div class="modal-content">
+            <div class="modal-header">
+                {{--<button type="button" class="close top-close" data-dismiss="modal" id="close-button">×</button>
+                <h4 class="modal-title title text-center fees_title" id="MasterTitle"><b>Class 6 General:</b> admission-fees</h4>--}}
+            </div>
+           
+            <div class="modal-footer">
+                <div class="box-body">
+                    <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Cancel</button>
+                    <button type="submit" class="btn cfees save_button btn-success" id="load" data-loading-text="<i class='fa fa-circle-o-notch fa-spin'></i> Processing"> <i class="fa fa-bed" aria-hidden="true"></i> Assign Bed </button>
+                </div>
+            </div>
+        </div>
+        {!! Form::close() !!}
+
+    </div>
+</div>
