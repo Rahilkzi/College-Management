17095ebd85dd1611e0030a195aa26708f41076a3
Rahilkazi <rahilkazi66@gmail.com>
2025-02-15 21:38:43 +0530

    document upload in edit and registrtion form

--- resources/views/student/registration/edit.blade.php
+++ resources/views/student/registration/edit.blade.php
@@ -30,12 +30,12 @@
                             </a>
                         </div>
                         {!! Form::model($data['row'], [
-    'route' => [$base_route . '.update', $data['row']->id],
-    'method' => 'POST',
-    'class' => 'form-horizontal',
-    'id' => 'validation-form',
-    "enctype" => "multipart/form-data"
-]) !!}
+        'route' => [$base_route . '.update', $data['row']->id],
+        'method' => 'POST',
+        'class' => 'form-horizontal',
+        'id' => 'validation-form',
+        "enctype" => "multipart/form-data"
+    ]) !!}
                         {!! Form::hidden('id', $data['row']->id) !!}
                         {{--{!! Form::hidden('guardians_id', $data['row']->guardians_id) !!}--}}
                         @include($view_path . '.registration.includes.form')
@@ -82,74 +82,99 @@
                 .catch(error => console.error("Error in transliteration:", error));
         }
     </script>
+
     <script>
         $(document).ready(function () {
-            $('.select2').select2({
-                placeholder: "Select Document Type",
-                allowClear: true
+                $('.select2').select2({
+                    placeholder: "Select Document Type",
+                    allowClear: true
+                });
             });
-        });
 
-        function addDocumentField() {
-            let wrapper = document.getElementById('document-wrapper1');
-            let html = `
-                    <div class="col-xs-12 col-sm-8">
-                    <div class="document-form-group">
-                                    <div class="form-group">
-                                        <label class="col-sm-3 control-label no-padding-right">Document Type</label>
-                                        <div class="col-sm-9">
-                                            <select name="title[]" class="form-control select2" required>
-                                                <option value="">Select Document Type</option>
-                                                <optgroup label="Academic Documents">
-                                                    <option value="marksheet_10">10th Marksheet</option>
-                                                    <option value="marksheet_12">12th Marksheet</option>
-                                                    <option value="transfer_certificate">Transfer Certificate</option>
-                                                </optgroup>
-                                                <optgroup label="Personal Documents">
-                                                    <option value="birth_certificate">Birth Certificate</option>
-                                                    <option value="medical_certificate">Medical Certificate</option>
-                                                    <option value="nationality_certificate">Nationality Certificate</option>
-                                                </optgroup>
-                                                <optgroup label="Other Certificates">
-                                                    <option value="bonafide_certificate">Bonafide Certificate</option>
-                                                    <option value="character_certificate">Character Certificate</option>
-                                                    <option value="migration_certificate">Migration Certificate</option>
-                                                    <option value="gap_certificate">Gap Certificate</option>
-                                                </optgroup>
-                                            </select>
+            function addDocumentField() {
+                let wrapper = document.getElementById('document-wrapper1');
+                let html = `
+                        <div class="col-xs-12 col-sm-8">
+                        <div class="document-form-group">
+                                        <div class="form-group">
+                                            <label class="col-sm-3 control-label no-padding-right">Document Type</label>
+                                            <div class="col-sm-9">
+                                                <select name="title[]" class="form-control select2" required>
+                                                    <option value="">Select Document Type</option>
+                                                    <optgroup label="Academic Documents">
+                                                        <option value="marksheet_10">10th Marksheet</option>
+                                                        <option value="marksheet_12">12th Marksheet</option>
+                                                        <option value="transfer_certificate">Transfer Certificate</option>
+                                                    </optgroup>
+                                                    <optgroup label="Personal Documents">
+                                                        <option value="birth_certificate">Birth Certificate</option>
+                                                        <option value="medical_certificate">Medical Certificate</option>
+                                                        <option value="nationality_certificate">Nationality Certificate</option>
+                                                    </optgroup>
+                                                    <optgroup label="Other Certificates">
+                                                        <option value="bonafide_certificate">Bonafide Certificate</option>
+                                                        <option value="character_certificate">Character Certificate</option>
+                                                        <option value="migration_certificate">Migration Certificate</option>
+                                                        <option value="gap_certificate">Gap Certificate</option>
+                                                    </optgroup>
+                                                </select>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="col-sm-3 control-label no-padding-right">Upload File</label>
-                                        <div class="col-sm-9">
-                                            <div class="input-group">
-                                                <span class="input-group-addon">
-                                                    <i class="ace-icon fa fa-upload"></i>
-                                                </span>
-                                                <input type="file" name="document_file[]" class="form-control file-upload" required>
+                                        <div class="form-group">
+                                            <label class="col-sm-3 control-label no-padding-right">Upload File</label>
+                                            <div class="col-sm-9">
+                                                <div class="input-group">
+                                                    <span class="input-group-addon">
+                                                        <i class="ace-icon fa fa-upload"></i>
+                                                    </span>
+                                                    <input type="file" name="document_file[]" class="form-control file-upload" required>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="col-sm-3 col-sm-offset-9">
+                                                <button type="button" class="btn btn-danger btn-sm" onclick="removeDocumentField(this)">
+                                                    <i class="fa fa-trash"></i> Remove
+                                                </button>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="form-group">
-                                        <div class="col-sm-3 col-sm-offset-9">
-                                            <button type="button" class="btn btn-danger btn-sm" onclick="removeDocumentField(this)">
-                                                <i class="fa fa-trash"></i> Remove
-                                            </button>
-                                        </div>
-                                    </div>
                                 </div>
-                            </div>
 
-                `;
-            wrapper.insertAdjacentHTML('beforeend', html);
+                    `;
+                wrapper.insertAdjacentHTML('beforeend', html);
 
-            // Reinitialize select2 for new fields
-            $('.select2').select2({
-                placeholder: "Select Document Type",
-                allowClear: true
-            });
-        }
+                // Reinitialize select2 for new fields
+                $('.select2').select2({
+                    placeholder: "Select Document Type",
+                    allowClear: true
+                });
+            }
 
-        // ... rest of your existing JS functions ...
-    </script>
+            function removeDocumentField(button) {
+                button.closest('.document-form-group').remove();
+            }
+
+            function deleteDocument(documentId) {
+                if (confirm('Are you sure you want to delete this document?')) {
+                    $.ajax({
+                        url: '/student/document/' + documentId,
+                        type: 'DELETE',
+                        data: {
+                            _token: '{{ csrf_token() }}'
+                        },
+                        success: function (response) {
+                            if (response.success) {
+                                location.reload();
+                            } else {
+                                alert('Failed to delete document');
+                            }
+                        },
+                        error: function () {
+                            alert('An error occurred while deleting the document');
+                        }
+                    });
+                }
+            }
+        </script>
 @endsection
