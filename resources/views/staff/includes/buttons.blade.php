1a806bb576fd1e9658630bed684c81097ffeb2aa
Rahilkazi <rahilkazi66@gmail.com>
2025-03-28 18:25:33 +0530

    fix staff page remove service activation and broken route

--- resources/views/staff/includes/buttons.blade.php
+++ resources/views/staff/includes/buttons.blade.php
@@ -6,7 +6,6 @@
         <a class="{!! request()->is('staff/document*')?'btn-success':'btn-primary' !!} btn-sm" href="{{ route('staff.document') }}"><i class="fa fa-files-o" aria-hidden="true"></i>&nbsp;Documents</a>
         <a class="{!! request()->is('staff/note*')?'btn-success':'btn-primary' !!} btn-sm" href="{{ route('staff.note') }}"><i class="fa fa-sticky-note" aria-hidden="true"></i>&nbsp;Notes</a>
         <a class="{!! request()->is('account/payroll/balance')?'btn-success':'btn-primary' !!} btn-sm" href="{{ route('account.payroll.balance') }}"><i class="fa fa-calculator" aria-hidden="true"></i>&nbsp;Payroll</a>
-        <a class="{!! request()->is('library/staff*')?'btn-success':'btn-primary' !!} btn-sm" href="{{ route('library.staff') }}"><i class="fa fa-book" aria-hidden="true"></i> Library</a>
         <a class="{!! request()->is('attendance/staff*')?'btn-success':'btn-primary' !!} btn-sm" href="{{ route('attendance.staff') }}"><i class="fa fa-calendar" aria-hidden="true"></i> Attendance</a>
         <a class="{!! request()->is('staff/designation*')?'btn-success':'btn-primary' !!} btn-sm" href="{{ route('staff.designation') }}"><i class="fa fa-star-half-full" aria-hidden="true"></i> Designation</a>
     </div>
